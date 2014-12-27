6038afe1a13ebcf50a376a558e92efc79f27aadc
John van Weel <jvweel@tricode.nl>
2014-12-27 12:17:48 +0100

    First commit

---
+++ app/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name twimp3App
+ * @description
+ * # twimp3App
+ *
+ * Main module of the application.
+ */
+angular
+  .module('twimp3App', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
